d72e296069ffc0bb747e3eed2d0b7f3905cc0755
Gizem <gizemwanka@gmail.com>
2021-04-10 15:03:21 +0200

    updated branch

--- models/update-petition.model.js
+++ models/update-petition.model.js
@@ -1,24 +1,24 @@
 const mongoose = require("mongoose");
 
-const updatePeititionSchema = new mongoose.Schema({
-  photo: {
-    type: String,
-  },
-  date: Date,
+const updatePetitionSchema = new mongoose.Schema({
+    photo: {
+        type: String,
+    },
+    date: Date,
 
-  title: {
-    type: String,
-    required: true,
-    min: 15,
-    max: 200,
-  },
-  description: {
-    type: String,
-    required: true,
-    min: 50,
-  },
+    title: {
+        type: String,
+        required: true,
+        min: 15,
+        max: 200,
+    },
+    description: {
+        type: String,
+        required: true,
+        min: 50,
+    },
 });
 
-const updatePetition = mongoose.model("updatePetition", updatePeititionSchema);
+const updatePetition = mongoose.model("updatePetition", updatePetitionSchema);
 
 module.exports = updatePetition;
