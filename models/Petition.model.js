45ac4bd56b1db28a202a18c68c2394159c0abdc7
Bclark <brittneyclark39@gmail.com>
2021-04-10 14:57:57 +0200

    capitalize

--- models/Petition.model.js
+++ models/Petition.model.js
@@ -3,37 +3,37 @@ const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
 const petitionSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    subject: {
-        type: String,
-        min: 10,
-        max: 200,
-        required: true,
-    },
-    description: {
-        type: String,
-        required: true,
-        min: 30,
-    },
-    deadline: Date,
-    signatures: [{ type: Schema.Types.ObjectId, ref: "User" }],
-    location: {
-        type: String,
-    },
-    organizer: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-    },
-    signatureTarget: {
-        type: Number,
-    },
-    picture: {
-        type: String,
-        default: "",
-    },
+  name: {
+    type: String,
+    required: true,
+  },
+  subject: {
+    type: String,
+    min: 10,
+    max: 200,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    min: 30,
+  },
+  deadline: Date,
+  signatures: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  location: {
+    type: String,
+  },
+  organizer: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  signatureTarget: {
+    type: Number,
+  },
+  picture: {
+    type: String,
+    default: "",
+  },
 });
 
 const Petition = model("Petition", petitionSchema);
