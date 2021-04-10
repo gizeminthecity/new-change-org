e820a2f66fad5fe20ba630a4f9dd8aace09b373e
Gizem <gizemwanka@gmail.com>
2021-04-10 12:36:32 +0200

    add Petition model

---
+++ models/Petition.model.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const { Schema, model } = mongoose;
+
+const petitionSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    subject: {
+        type: String,
+        min: 10,
+        // max: 150,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+        min: 30,
+    },
+    deadline: Date,
+    signatures: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    location: {
+        type: String,
+    },
+    organizer: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
+    signatureTarget: {
+        type: Number,
+    },
+    picture: {
+        type: String,
+        default: "",
+    },
+});
+
+const Petition = model("Petition", petitionSchema);
+
+module.exports = Petition;
