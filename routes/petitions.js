98fac8e61e6c4b8af0cdbc4ac6904ddd29f580e4
Gizem <gizemwanka@gmail.com>
2021-04-17 14:25:09 +0200

    updates

--- routes/petitions.js
+++ routes/petitions.js
@@ -4,6 +4,7 @@ const Petition = require("../models/Petition.model");
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
+    console.log("req.query", req.query);
     Petition.find().then((allPetitions) => {
         console.log("allPetitions: ", allPetitions[0]);
         res.render("petitions", { petitionList: allPetitions });
