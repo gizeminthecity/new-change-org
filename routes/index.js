804b11d7f4685ca7f25295745f6aecf939188efd
Bclark <brittneyclark39@gmail.com>
2021-04-19 22:04:43 +0200

    working starting to code routes for user

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,12 @@ const router = require("express").Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-    let user;
-    if (req.session.user) {
-        user = req.session.user;
-    }
-    console.log(req.session.user);
-    res.render("index");
+  let user;
+  if (req.session.user) {
+    user = req.session.user;
+  }
+  console.log(req.session.user);
+  res.render("index");
 });
 
 module.exports = router;
-
-//brittneys first pull request//
