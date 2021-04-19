e04e9073beeb1736151cc1ad65de035b5a13bc34
Bclark <brittneyclark39@gmail.com>
2021-04-19 23:50:27 +0200

    almost done with updating profile

--- routes/profile.js
+++ routes/profile.js
@@ -1,7 +1,23 @@
 const router = require("express").Router();
-const User = require("../models/User.model.js");
+const User = require("../models/User.model");
+const isLoggedMiddleware = require("../middlewares/isLoggedIn");
 
-router.get("/", (req, res, next) => {
-    res.render("profile");
+router.get("/", isLoggedMiddleware, (req, res) => {
+  res.render("profile", { user: req.session.user });
+});
+
+router.get("/edit", isLoggedMiddleware, (req, res) => {
+  res.render("edit-profile", { user: req.session.user });
+});
+
+router.post("/edit", isLoggedMiddleware, (req, res) => {
+  const { username, bio } = req.body;
+
+  user
+    .findByIdAndUpdate(req.session.user._id, { username, bio }, { new: true })
+    .then((newUser) => {
+      req.session.user = newUser;
+      res.redirect("/profile");
+    });
 });
 module.exports = router;
