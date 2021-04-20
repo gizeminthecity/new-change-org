88390004085ca9457ee5ea78f39310626a083e92
Bclark <brittneyclark39@gmail.com>
2021-04-20 21:12:53 +0200

    ongoing work

--- routes/profile.js
+++ routes/profile.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const User = require("../models/User.model");
 const isLoggedMiddleware = require("../middlewares/isLoggedIn");
+const Petition = require("../models/Petition.model");
+const UpdatePetition = require("../models/UpdatePetition.model");
 
 router.get("/", isLoggedMiddleware, (req, res) => {
   res.render("profile", { user: req.session.user });
@@ -23,20 +25,55 @@ router.post("/edit", isLoggedMiddleware, (req, res) => {
   });
 });
 
-//router.get("/delete", isLoggedMiddleware, (req, res) => {
-//User.findByIdAndDelete(req.session.user._id).then(() => {
-// return res.redirect("/");
-//});
-//});
+router.get("/delete", isLoggedMiddleware, async (req, res) => {
+  const userId = req.session.user._id;
+  // Delete the user
+  // await User.findByIdAndDelete(userId);
 
-//rounter.get("/:dynamic/delete", isLoggedMiddleware, (req, res) =>{
-// User.findById(req.params.dynamic).then() => {
-//   if(!user) {
-//     return res.redirect("/auth/login");
-//}
-//User.findByIdAndDelete(req.session.user._id).then() => {
-//  return res.redirect("/")
-//}
-//}
-//})
+  const allPetitionsByUser = await Petition.find({ owner: userId });
+
+  console.log("allPetitions byIser:", allPetitionsByUser);
+
+  // array of all of the update petitions for each single petition created by a user
+  const updatePetitions = allPetitionsByUser.map((e) =>
+    UpdatePetition.find({ parent: e._id })
+  );
+
+  const allUpdatePetitions = await Promise.all(updatePetitions);
+  console.log("updates: ", allUpdatePetitions);
+
+  res.render("profile");
+
+  // //  listing of all petitions by the current user
+  // const allPetitionsByUser = await Petition.find({
+  //   owner: req.session.user._id,
+  // })
+
+  // const arrayOfPetitionIds = allPetitionsByUser.map(e => e._id)
+
+  // // it bulk deletes all of the previously defined petitions
+  // await Petition.deleteMany({
+  //   _id: { $in: arrayOfPetitionIds },
+  // });
+
+  // // listing of all of the petitions in the db, that the user signed
+  // const everyPetitionSignedByUser = await Petition.find({
+  //   signatures: { $in: userId },
+  // });
+
+  // // array of promises where we intruct to remove the userId from signatures
+  // const updateAllPetitions = everyPetitionSignedByUser.map((e) =>
+  //   Petition.findByIdAndUpdate(e._id, { $pull: { signatures: userId } })
+  // );
+
+  // // run the promises defined above
+  // await Promise.all(updateAllPetitions);
+
+  // const allUpdates = await UpdatePetition.find({parent: })
+
+  // User.findByIdAndDelete(req.session.user._id).then((deletedUser) => {
+  //   res.redirect("/");
+  // });
+});
+
 module.exports = router;
