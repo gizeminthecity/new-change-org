ab604c040dc08309afe47f8d3fa2176ef9a48d85
Gizem <gizemwanka@gmail.com>
2021-04-23 20:31:47 +0200

    add signatures route

--- routes/petitions.js
+++ routes/petitions.js
@@ -31,6 +31,7 @@ router.get("/my-petitions", isLoggedIn, (req, res) => {
 router.get("/", (req, res) => {
     Petition.find().then((allPetitions) => {
         res.render("petitions", { petitionList: allPetitions });
+        // console.log("petitionlist:", allPetitions);
     });
 });
 
@@ -211,16 +212,33 @@ router.get("/:_id/remove", isLoggedIn, (req, res) => {
         { new: true }
     )
         .then((removedSignature) => {
-            console.log("foundPetition: ", removedSignature);
+            // console.log("foundPetition: ", removedSignature);
             let isRemoved = false;
 
             if (!removedSignature.signatures.includes(req.session.user._id)) {
                 isRemoved = true;
-                console.log(isRemoved);
+                // console.log(isRemoved);
             }
             res.render("single-petition", { isRemoved });
         })
         .catch((err) => console.log(err));
 });
 
+router.get("/:_id/signed", isLoggedIn, (req, res) => {
+    Petition.findById(req.params._id)
+        .populate("signatures")
+        .then((foundPetition) => {
+            console.log("foundPetition: ", foundPetition);
+            let signatureIdsArray = foundPetition.signatures;
+            const signatureUsernames = signatureIdsArray.map(
+                (el) => el.username
+            );
+            console.log("Usernames: ", signatureUsernames);
+
+            res.render("single-petition", {
+                foundPetition: foundPetition,
+                signatureUsernames,
+            });
+        });
+});
 module.exports = router;
